017a4b709bf421c88ff1476d76b3a9155f843bdf
AdamBadagliacco <adambadagliacco@gmail.com>
2022-06-20 17:43:09 -0500

    Added Attacks and Manager
    TODO Next:
    
    1) Add Audio to attacks so it can be used to Box. (Headphones tell you the attack to use)
    
    2) Clean Up a bit.
    Ex. Make a variable for how much speed decreases. And fix the Names/Number toggle by removing the both and having it use use between names and number which is checked. Break and error message if neither is checked.

---
+++ Boxing-Practice/Assets/Scripts/BoxingAttack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxingAttack : MonoBehaviour
+{
+    public string attackName;
+    public int attackNumber;
+
+    //Sound Effect
+    public GameObject SoundEffectName;
+    public GameObject SoundEffectNumber;
+
+    public void PlaySoundEffectName()
+    {
+        float soundEffectLength = SoundEffectName.GetComponent<AudioSource>().clip.length;
+        GameObject soundEffectInstance = Instantiate(SoundEffectName, new Vector3(0, 0, 0), Quaternion.identity);
+        Destroy(soundEffectInstance, soundEffectLength);
+    }
+
+     public void PlaySoundEffectNumber()
+    {
+        float soundEffectLength = SoundEffectNumber.GetComponent<AudioSource>().clip.length;
+        GameObject soundEffectInstance = Instantiate(SoundEffectNumber, new Vector3(0, 0, 0), Quaternion.identity);
+        Destroy(soundEffectInstance, soundEffectLength);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Boxing-Practice/Assets/Scripts/BoxingPracticeManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxingPracticeManager : MonoBehaviour
+{
+    public List<GameObject> BoxingAttacks;
+
+    [Tooltip("Pick one of below three")]
+    [Header("Audio Cue Of Attacks")]
+    public bool numbers;
+    public bool names;
+    public bool both;
+
+    [Header("Other Attack Variables")]
+    public bool rollsOn;
+    public bool speedIncreases;
+    public float speed;
+    
+    [Header("Combos")]
+    public bool combosOn;
+    public int amountOfCombos;
+    public int hitsPerCombo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartBoxingPractice());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetRandomAttack(){
+        int randomNumber = Random.Range(0, BoxingAttacks.Count);
+        return BoxingAttacks[randomNumber];
+    }
+
+    public void PlaySoundEffectName(GameObject NextAttack){
+        Debug.Log(NextAttack.GetComponent<BoxingAttack>().attackName);                 
+        //Play Attack Name
+        //NextAttack.GetComponent<BoxingAttack>()PlaySoundEffectName();
+    }
+
+    public void PlaySoundEffectNumber(GameObject NextAttack){
+         Debug.Log(NextAttack.GetComponent<BoxingAttack>().attackNumber);           
+                //Play Attack Number
+                //NextAttack.GetComponent<BoxingAttack>()PlaySoundEffectNumber();
+    }
+
+    public IEnumerator StartBoxingPractice(){
+
+        bool on = true;
+
+        while(on){
+
+            GameObject NextAttack = GetRandomAttack();
+
+            if(numbers){
+                PlaySoundEffectNumber(NextAttack);  
+            }
+            else if(names){
+               PlaySoundEffectName(NextAttack);  
+            }
+            else{
+                int nameOrNumber = Random.Range(0, 1);
+                if(nameOrNumber == 0){
+                    PlaySoundEffectNumber(NextAttack);  
+                }
+                else{
+                    PlaySoundEffectName(NextAttack);  
+                }
+            }
+             yield return new WaitForSeconds(speed);
+             if(speedIncreases){
+                 speed =- 0.2f;
+             }
+        }
+    }
+}
